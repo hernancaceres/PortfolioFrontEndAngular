52575e78086a15e36d7a23af772f88db86182929
hernancaceres <harinatha777@hotmail.com>
2022-08-14 13:21:53 -0300

    version 1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule )
+  },
+
+  {
+    path: 'productos',
+    loadChildren: () => import('./productos/productos.module').then(m => m.ProductosModule )
+  },
+  
+  {
+    path: '**',
+    redirectTo: 'auth',
+    
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
